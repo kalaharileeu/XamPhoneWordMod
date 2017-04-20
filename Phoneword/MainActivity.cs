990faec237982322774fe0fd6789ef9437dfd7d8
Cornel <cfdelange@gmail.com>
2017-04-20 19:22:59 +1200

    First commit of example

---
+++ Phoneword/MainActivity.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+
+using Android.Widget;
+using Android.OS;
+using System.Text;
+
+namespace Phoneword
+{
+    public static class PhonewordTranslator
+    {
+        public static string ToNumber(string raw)
+        {
+            if (string.IsNullOrWhiteSpace(raw))
+                return "";
+            else
+                raw = raw.ToUpperInvariant();
+
+            var newNumber = new StringBuilder();
+            foreach (var c in raw)
+            {
+                if (" -0123456789".Contains(c))
+                    newNumber.Append(c);
+                else {
+                    var result = TranslateToNumber(c);
+                    if (result != null)
+                        newNumber.Append(result);
+                }
+                // otherwise we've skipped a non-numeric char
+            }
+            return newNumber.ToString();
+        }
+        static bool Contains(this string keyString, char c)
+        {
+            return keyString.IndexOf(c) >= 0;
+        }
+        static int? TranslateToNumber(char c)
+        {
+            if ("ABC".Contains(c))
+                return 2;
+            else if ("DEF".Contains(c))
+                return 3;
+            else if ("GHI".Contains(c))
+                return 4;
+            else if ("JKL".Contains(c))
+                return 5;
+            else if ("MNO".Contains(c))
+                return 6;
+            else if ("PQRS".Contains(c))
+                return 7;
+            else if ("TUV".Contains(c))
+                return 8;
+            else if ("WXYZ".Contains(c))
+                return 9;
+            return null;
+        }
+    }
+    [Activity(Label = "Phone Word", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            // Our code will go here
+            // Get our UI controls from the loaded layout:
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            Button callButton = FindViewById<Button>(Resource.Id.CallButton);
+
+            // Disable the "Call" button
+            callButton.Enabled = false;
+
+            // Add code to translate number
+            string translatedNumber = string.Empty;
+
+            translateButton.Click += (object sender, EventArgs e) =>
+            {
+                // Translate user's alphanumeric phone number to numeric
+                translatedNumber = PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                if (String.IsNullOrWhiteSpace(translatedNumber))
+                {
+                    callButton.Text = "Call";
+                    callButton.Enabled = false;
+                }
+                else
+                {
+                    callButton.Text = "Call " + translatedNumber;
+                    callButton.Enabled = true;
+                }
+            };
+
+            callButton.Click += (object sender, EventArgs e) =>
+            {
+                // On "Call" button click, try to dial phone number.
+                var callDialog = new AlertDialog.Builder(this);
+                callDialog.SetMessage("Call " + translatedNumber + "?");
+                callDialog.SetNeutralButton("Call", delegate {
+                    // Create intent to dial phone
+                    var callIntent = new Intent(Intent.ActionCall);
+                    callIntent.SetData(Android.Net.Uri.Parse("tel:" + translatedNumber));
+                    StartActivity(callIntent);
+                });
+                callDialog.SetNegativeButton("Cancel", delegate { });
+
+                // Show the alert dialog to the user and wait for response.
+                callDialog.Show();
+            };
+        }
+    }
+}
+
